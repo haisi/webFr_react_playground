e58d551526f5b27f554de42bc54e6187f2110d6d
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-22 15:57:59 +0200

    Impl Hello to {name}

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.names = [
+            'Balin', 'Dwalin', 'Fili', 'Kili', 'Dori', 'Nori', 'Ori', 'Oin', 'Gloin',
+            'Bifur', 'Bofur', 'Bombur'
+        ];
+        this.state = {
+            index: 0
+        };
+
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        // this.state.index = this.state.index + 1; // Doesn't work
+        this.setState({index: (this.state.index + 1) % this.names.length});
+    }
+
+    render() {
+        return (<h1>{this.props.message} {this.names[this.state.index]}</h1>)
+    }
 }
 
 export default App;
